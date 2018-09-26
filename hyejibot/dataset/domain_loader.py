a126064e187b04f5c7fce5411effe9e74e56287b
Hyeji <137373@gmail.com>
2018-09-26 19:05:02 +0900

    data

---
+++ hyejibot/dataset/domain_loader.py
@@ -0,0 +1,14 @@
+import torch.utils.data as data
+
+
+class DomainDataset(data.Dataset):
+    """Domain Custom Dataset"""
+    def __init__(self, root):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        return 0 #len(self.)
+
